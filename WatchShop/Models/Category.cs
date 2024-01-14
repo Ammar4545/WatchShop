e721c94a8e6fefeddbf916c3fe5809b926e781bc
Ammar Mahmoud <ammar.m777.2@gmail.com>
2024-01-14 11:10:50 +0200

    add category model

---
+++ WatchShop/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace WatchShop.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string NAme { get; set; }
+        public string Description { get; set; }
+    }
+}
--- WatchShop/Services/ProductService.cs
+++ WatchShop/Services/ProductService.cs
@@ -17,7 +17,8 @@ namespace WatchShop.Services
             new Product{Id=2,NAme="test2",Description="Timex Unisex Weekender 38mm Watch",ImageUrl="https://m.media-amazon.com/images/I/61xSjdpeU0L._AC_UL320_.jpg",Price=35,Specification=""},
             new Product{Id=3,NAme="test3",Description="Amazfit Band 5 Activity Fitness Tracker with Alexa Built-in, 15-Day Battery Life, Blood Oxygen, Heart Rate, Sleep & Stress Monitoring, 5 ATM Water Resistant",ImageUrl="https://m.media-amazon.com/images/I/61xSjdpeU0L._AC_UL320_.jpg",Price=35,Specification=""},
             new Product{Id=4,NAme="test4",Description="OUPINKE Skeleton Watches Mens Automatic Mechanical Self Winding Wrist Watch Luxury Sapphire Crystal Tungsten Steel Waterproof Luminous Premium Dress Watch",ImageUrl="https://m.media-amazon.com/images/I/61xSjdpeU0L._AC_UL320_.jpg",Price=35,Specification=""},
-            new Product{Id=5,NAme="test5",Description="OUPINKE Men's Wrist Watches Automatic Mechanical Luxury Skeleton Self Winding Sapphire Crystal Waterproof Tungsten Steel Band",ImageUrl="https://m.media-amazon.com/images/I/61xSjdpeU0L._AC_UL320_.jpg",Price=35,Specification=""}
+            new Product{Id=5,NAme="test5",Description="OUPINKE Men's Wrist Watches Automatic Mechanical Luxury Skeleton Self Winding Sapphire Crystal Waterproof Tungsten Steel Band",ImageUrl="https://m.media-amazon.com/images/I/61xSjdpeU0L._AC_UL320_.jpg",Price=35,Specification=""},
+            new Product{Id=6,NAme="test5",Description="OUPINKE Men's Wrist Watches Automatic Mechanical Luxury Skeleton Self Winding Sapphire Crystal Waterproof Tungsten Steel Band",ImageUrl="https://m.media-amazon.com/images/I/61xSjdpeU0L._AC_UL320_.jpg",Price=35,Specification=""}
             };
         }
         public List<Product> GetProducts() => _inMemoryProducts;
